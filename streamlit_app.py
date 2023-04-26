c10473eda1f58d2b24cbd0ea434159654d252ff9
rsllmcrg <130658160+rsllmcrg@users.noreply.github.com>
2023-04-26 08:28:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,27 +14,3 @@ def main():
         st.write("<p style='text-align: center; font-style: italic;'>The Golden Apple Snail Eggs Detection Application is a web application designed to detect the presence of Golden Apple Snail eggs in images. "
                  "The app uses image processing and machine learning techniques to identify and highlight the location of Golden Apple Snail eggs in the uploaded images.</p>", unsafe_allow_html=True)
         st.write("Please upload an image containing the rice field:")
-
-    option = st.sidebar.selectbox(
-        'Select an option:',
-        ('Drag and Drop', 'Take a Photo'))
-
-    if option == 'Drag and Drop':
-        uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-        if uploaded_file is not None:
-            image = Image.open(uploaded_file)
-            st.image(image, caption='Uploaded Image', use_column_width=True)
-
-    if option == 'Take a Photo':
-        st.write("Click the button below to take a photo using your device's camera.")
-        if st.button('Take a photo'):
-            image_placeholder = st.empty()
-            # This will open the device's camera and capture a photo
-            # The captured photo will be displayed in the app
-            st.write("Taking photo...")
-            image_upload = st.file_uploader(" ", type=["jpg", "jpeg", "png"], key="camera")
-            if image_upload is not None:
-                file_bytes = io.BytesIO(image_upload.read())
-                image = Image.open(file_bytes)
-                image.save('captured_image.png')
-                image_placeholder.image(image, caption='Captured Image', use_column_width=True)
