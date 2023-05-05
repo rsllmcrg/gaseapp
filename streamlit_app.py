732922555dae5f3262cf9288c4fc4b935d6e2b42
rsllmcrg <130658160+rsllmcrg@users.noreply.github.com>
2023-05-05 17:00:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,52 +1,38 @@
 import streamlit as st
-from PIL import Image
-import io
-import requests
 import cv2
+import numpy as np
+from PIL import Image
 
+# Define a function to display an image
+def show_image(img):
+    st.image(img, use_column_width=True)
 
-def main():
-    st.set_page_config(page_title="Golden Apple Snail Eggs Detection", page_icon=":guardsman:", layout="wide")
-    st.markdown("<h1 style='text-align: center;'>Golden Apple Snail Eggs Detection</h1>", unsafe_allow_html=True)
+# Define a function to capture an image from the user's webcam
+def capture_image():
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    cap.release()
+    return frame
 
-    # Create a menu with multiple pages
-    menu = ["Home", "About"]
-    choice = st.sidebar.selectbox("Select a page", menu)
+# Create a Streamlit web app
+st.title("Image Uploader")
 
-    if choice == "Home":
-        st.write("<p style='text-align: center; font-style: italic;'>The Golden Apple Snail Eggs Detection Application is a web application designed to detect the presence of Golden Apple Snail eggs in images. "
-                 "The app uses image processing and machine learning techniques to identify and highlight the location of Golden Apple Snail eggs in the uploaded images.</p>", unsafe_allow_html=True)
-        st.write("Please upload an image containing the rice field:")
+# Create a button for uploading an image using drag and drop
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"], drag_and_drop=True)
+if uploaded_file is not None:
+    # Use the PIL library to read the image data
+    image = Image.open(uploaded_file)
 
-        option = st.selectbox("", ("Drag and drop a photo", "Take a photo"))
+    # Display the uploaded image
+    show_image(image)
 
-        if option == "Drag and drop a photo":
-            st.write("Please drag and drop a photo below:")
-            image_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
-            if image_file is not None:
-                img = Image.open(image_file)
-                st.image(img, caption="Uploaded photo", use_column_width=True)
+# Create a button for taking a photo using the user's webcam
+if st.button("Take a Photo"):
+    # Capture an image from the user's webcam
+    frame = capture_image()
 
-        elif option == "Take a photo":
-            def take_photo():
-                cap = cv2.VideoCapture(0)
-                ret, frame = cap.read()
-                cap.release()
-                return frame
+    # Convert the image from OpenCV format to PIL format
+    image = Image.fromarray(np.uint8(frame))
 
-            uploaded_file = st.sidebar.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
-
-
-    elif choice == "About":
-        st.write("""
-
-        The Golden Apple Snail Eggs Detection Application is a computer vision-based application that aims to detect the presence of golden apple snail eggs in rice fields. The golden apple snail is a notorious pest in rice fields, and its eggs can cause significant damage to crops. This application provides a quick and efficient way to detect the presence of these eggs, allowing farmers to take appropriate measures to prevent further damage.
-
-        The application is built using deep learning techniques and is trained on a large dataset of images of golden apple snail eggs. It uses convolutional neural networks (CNNs) to automatically extract features from the input images and classify them as either containing or not containing golden apple snail eggs. The application can be run on a desktop computer or a mobile device and can be accessed through a user-friendly interface.
-
-        The Golden Apple Snail Eggs Detection Application has the potential to revolutionize the way farmers detect and prevent golden apple snail infestations. By providing a fast and accurate way to detect the presence of snail eggs, the application can help farmers save time and money and reduce the use of harmful pesticides.
-        """)
-
-
-if __name__ == '__main__':
-    main()
+    # Display the captured image
+    show_image(image)
