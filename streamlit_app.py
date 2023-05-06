53c7baed54e35196bf986cd1eafd1216739048e7
rsllmcrg <130658160+rsllmcrg@users.noreply.github.com>
2023-05-06 08:19:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ from PIL import Image
 
 def main():
     st.set_page_config(page_title="Golden Apple Snail Eggs Detection", page_icon=":guardsman:", layout="wide")
-    st.markdown("<h1 style='text-align: center;'>Golden Apple Snail Eggs Detection</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='display: flex; align-items: center;'><img src='https://example.com/image.png' style='margin-right: 10px;'>Golden Apple Snail Eggs Detection</h1>", unsafe_allow_html=True)
+
 
     # Create a top menu
     menu = ["Home", "About"]
