270db742a8e9f9d28750fb4caa1895657193553d
rsllmcrg <130658160+rsllmcrg@users.noreply.github.com>
2023-05-06 11:06:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,9 @@ def get_image_location(image):
     return None
 
 def main():
-    # Rest of your code...
+    # Create a menu with multiple pages
+    menu = ["Home", "About"]
+    choice = st.sidebar.selectbox("Select a page", menu)
 
     if choice == "Home":
         st.write("<p style='text-align: center; font-style: italic;'>The Golden Apple Snail Eggs Detection Application is a web application designed to detect the presence of Golden Apple Snail eggs in images. "
@@ -47,8 +49,6 @@ def main():
                 else:
                     st.write("Location information not found in the image metadata.")
 
-        # Rest of your code...
-
     elif choice == "About":
         st.write("""
 
@@ -56,9 +56,4 @@ def main():
 
         The application is built using deep learning techniques and is trained on a large dataset of images of golden apple snail eggs. It uses convolutional neural networks (CNNs) to automatically extract features from the input images and classify them as either containing or not containing golden apple snail eggs. The application can be run on a desktop computer or a mobile device and can be accessed through a user-friendly interface.
 
-        The Golden Apple Snail Eggs Detection Application has the potential to revolutionize the way farmers detect and prevent golden apple snail infestations. By providing a fast and accurate way to detect the presence of snail eggs, the application can help farmers save time and money and reduce the use of harmful pesticides.
-        """)
-
-
-if __name__ == '__main__':
-    main()
+        The Golden Apple Snail Eggs Detection Application has the potential to revolutionize the way farmers detect and prevent
