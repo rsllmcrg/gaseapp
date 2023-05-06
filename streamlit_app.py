5476f84045624e5b416f5842c721f628139758fa
rsllmcrg <130658160+rsllmcrg@users.noreply.github.com>
2023-05-06 11:17:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,9 +53,22 @@ def main():
                 else:
                     st.write("Location information not found in the image metadata.")
 
+        elif option == "Camera":
+            st.write("Please capture a photo:")
+            image_file = st.camera_input("")
+            if image_file is not None:
+                img = Image.open(image_file)
+                st.image(img, caption="Captured photo", use_column_width=True)
+
+                # Get the location of the captured image
+                location = get_image_location(img)
+                if location:
+                    st.write("Image location:", location)
+                else:
+                    st.write("Location information not found in the image metadata.")
+
     elif choice == "About":
-        st.write("""
-        The Golden Apple Snail Eggs Detection Application is a computer vision-based application that aims to detect the presence of golden apple snail eggs in rice fields. The golden apple snail is a notorious pest in rice fields, and its eggs can cause significant damage to crops. This application provides a quick and efficient way to detect the presence of these eggs, allowing farmers to take appropriate measures to prevent further damage.
+        st.write("""The Golden Apple Snail Eggs Detection Application is a computer vision-based application that aims to detect the presence of golden apple snail eggs in rice fields. The golden apple snail is a notorious pest in rice fields, and its eggs can cause significant damage to crops. This application provides a quick and efficient way to detect the presence of these eggs, allowing farmers to take appropriate measures to prevent further damage.
 
         The application is built using deep learning techniques and is trained on a large dataset of images of golden apple snail eggs. It uses convolutional neural networks (CNNs) to automatically extract features from the input images and classify them as either containing or not containing golden apple snail eggs. The application can be run on a desktop computer or a mobile device and can be accessed through a user-friendly interface.
 
