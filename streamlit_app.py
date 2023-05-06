48d8c290fa4d2323d8425ba9cc7a7186e7122da4
rsllmcrg <130658160+rsllmcrg@users.noreply.github.com>
2023-05-06 08:05:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,27 +40,4 @@ def show_main_page():
 
         if option == "Upload":
             st.write("Please drag and drop a photo below:")
-            image_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
-            if image_file is not None:
-                img = Image.open(image_file)
-                st.image(img, caption="Uploaded photo", use_column_width=True)
-
-        elif option == "Camera":
-            st.write("Please drag and drop a photo below:")
-            image_file = st.camera_input("")
-            if image_file is not None:
-                img = Image.open(image_file)
-                st.image(img, caption="Uploaded photo", use_column_width=True)
-
-        uploaded_file = st.sidebar.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
-
-        if uploaded_file is not None:
-            image = Image.open(uploaded_file)
-            st.image(image, caption='Uploaded image', use_column_width=True)
-
-    elif choice == "About":
-        st.write("""
-
-        The Golden Apple Snail Eggs Detection Application is a computer vision-based application that aims to detect the presence of golden apple snail eggs in rice fields. The golden apple snail is a notorious pest in rice fields, and its eggs can cause significant damage to crops. This application provides a quick and efficient way to detect the presence of these eggs, allowing farmers to take appropriate measures to prevent further damage.
-
-        The application is built using deep learning techniques and is trained on a large dataset of images of golden apple snail eggs. It uses convolutional neural networks (CNNs) to automatically extract features from the input images and classify them as either containing or not containing golden apple snail eggs. The application can be run on a desktop computer or a mobile device and
+            image
