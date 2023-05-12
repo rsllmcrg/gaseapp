fc7cd295b801a0be7636260e789f19fd9217cb71
rsllmcrg <130658160+rsllmcrg@users.noreply.github.com>
2023-05-12 14:01:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,61 +1,31 @@
 import streamlit as st
-from PIL import Image
+import piexif
 
-# Function to extract and convert GPS coordinates to decimal degrees
-def get_gps_coordinates(exif_data):
-    gps_info = exif_data.get(34853)  # GPSInfo tag for GPS data
-    
-    if gps_info is None:
-        return None
-    
-    # Extract GPS coordinates
-    lat_values = gps_info[2]
-    lon_values = gps_info[4]
-    
-    # Convert degrees/minutes/seconds to decimal degrees
-    latitude = lat_values[0] + lat_values[1] / 60 + lat_values[2] / 3600
-    longitude = lon_values[0] + lon_values[1] / 60 + lon_values[2] / 3600
-    
-    # Handle negative coordinates
-    if gps_info[1] == 'S':
-        latitude = -latitude
-    if gps_info[3] == 'W':
-        longitude = -longitude
-    
-    return latitude, longitude
+def add_exif_data(image_data):
+    # Create an empty EXIF dictionary
+    exif_dict = {"0th": {}, "Exif": {}}
 
-# Streamlit web app
+    # Add desired EXIF data to the dictionary
+    # Example: Adding a custom image description
+    exif_dict["0th"][piexif.ImageIFD.ImageDescription] = "My Custom Description"
+
+    # Convert the EXIF dictionary to bytes
+    exif_bytes = piexif.dump(exif_dict)
+
+    # Insert the EXIF data into the image
+    image_data = piexif.insert(exif_bytes, image_data)
+
+    return image_data
+
 def main():
-    st.title("GPS Coordinates Extractor")
-    
-    # Upload image file
-    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg"])
-    
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Photo", use_column_width=True)
-        
-        # Extract Exif metadata
-        exif_data = image._getexif()
-        
-        # Check if Exif metadata exists and has GPS information
-        if exif_data is not None and 34853 in exif_data:
-            # Extract and convert GPS coordinates
-            latitude, longitude = get_gps_coordinates(exif_data)
-            
-            if latitude is not None and longitude is not None:
-                st.subheader("GPS Coordinates")
-                
-                if isinstance(latitude, float) and isinstance(longitude, float):
-                    st.write(f"Latitude: {latitude:.6f}")
-                    st.write(f"Longitude: {longitude:.6f}")
-                else:
-                    st.write(f"Latitude: {latitude}")
-                    st.write(f"Longitude: {longitude}")
-            else:
-                st.write("No GPS coordinates found.")
-        else:
-            st.write("No GPS information found in Exif metadata.")
+    # Capture image using Streamlit
+    image_data = st.camera()
+
+    # Add EXIF data to the captured image
+    image_data = add_exif_data(image_data)
+
+    # Display the image with EXIF data
+    st.image(image_data, use_column_width=True)
 
 if __name__ == "__main__":
     main()
