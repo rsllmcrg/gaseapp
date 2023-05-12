be218ee59c2a3f5f426a4f85f1a1d50b00bc3a66
rsllmcrg <130658160+rsllmcrg@users.noreply.github.com>
2023-05-12 16:23:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,12 @@
 import streamlit as st
 
-# Inject custom CSS to hide the hamburger menu
-hide_menu_style = """
-<style>
-div.stButton button:first-child {
-    display: none;
-}
-</style>
-"""
-
-# Render the CSS
-st.markdown(hide_menu_style, unsafe_allow_html=True)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 # Your Streamlit app code goes here
 st.title("My Streamlit App")
