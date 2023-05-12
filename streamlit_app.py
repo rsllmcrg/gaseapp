62c8a17eb2c78bfb652f8a674d2478cce9517b9c
rsllmcrg <130658160+rsllmcrg@users.noreply.github.com>
2023-05-12 13:44:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,56 @@
 import streamlit as st
 from PIL import Image
-# import io
-import requests
-import cv2
 
+# Function to extract and convert GPS coordinates to decimal degrees
+def get_gps_coordinates(exif_data):
+    gps_info = exif_data.get(34853)  # GPSInfo tag for GPS data
+    
+    if gps_info is None:
+        return None
+    
+    # Extract GPS coordinates
+    lat_values = gps_info[2]
+    lon_values = gps_info[4]
+    
+    # Convert degrees/minutes/seconds to decimal degrees
+    latitude = lat_values[0] + lat_values[1] / 60 + lat_values[2] / 3600
+    longitude = lon_values[0] + lon_values[1] / 60 + lon_values[2] / 3600
+    
+    # Handle negative coordinates
+    if gps_info[1] == 'S':
+        latitude = -latitude
+    if gps_info[3] == 'W':
+        longitude = -longitude
+    
+    return latitude, longitude
+
+# Streamlit web app
 def main():
-    # Create a menu with multiple pages
-    menu = ["Home", "About"]
-    choice = st.sidebar.selectbox("Select a page", menu)
+    st.title("GPS Coordinates Extractor")
+    
+    # Upload image file
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg"])
+    
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Photo", use_column_width=True)
+        
+        # Extract Exif metadata
+        exif_data = image._getexif()
+        
+        # Check if Exif metadata exists and has GPS information
+        if exif_data is not None and 34853 in exif_data:
+            # Extract and convert GPS coordinates
+            latitude, longitude = get_gps_coordinates(exif_data)
+            
+            if latitude is not None and longitude is not None:
+                st.subheader("GPS Coordinates")
+                st.write(f"Latitude: {latitude:.6f}")
+                st.write(f"Longitude: {longitude:.6f}")
+            else:
+                st.write("No GPS coordinates found.")
+        else:
+            st.write("No GPS information found in Exif metadata.")
 
-    if choice == "Home":
-        st.markdown("<h1 style='text-align: center;'>GAS Egg Detector</h1>", unsafe_allow_html=True)
-        st.write("<p style='text-align: center; font-style: italic;'>Use these options to upload or capture Kuhol Eggs!</p>", unsafe_allow_html=True)
-        st.write("Please upload an image containing GAS eggs:")
-
-        option = st.radio("", ("Upload", "Camera"))
-  
-        if option == "Upload":
-            st.write("Please drag and drop a photo below:")
-            uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
-            if uploaded_file is not None:
-                # Read the uploaded file and convert it into an image object
-                img = Image.open(uploaded_file)
-                st.image(img, caption="Uploaded photo", use_column_width=True)
-                
-        elif option == "Camera":
-            st.write("Please drag and drop a photo below:")
-            image_file = st.camera_input("")
-            if image_file is not None:
-                img = Image.open(image_file)
-                st.image(img, caption="Uploaded photo", use_column_width=True)
-
-    elif choice == "About":
-        st.write("""
-
-        The Golden Apple Snail Eggs Detection Application is a computer vision-based application that aims to detect the presence of golden apple snail eggs in rice fields. The golden apple snail is a notorious pest in rice fields, and its eggs can cause significant damage to crops. This application provides a quick and efficient way to detect the presence of these eggs, allowing farmers to take appropriate measures to prevent further damage.
-
-        The application is built using deep learning techniques and is trained on a large dataset of images of golden apple snail eggs. It uses convolutional neural networks (CNNs) to automatically extract features from the input images and classify them as either containing or not containing golden apple snail eggs. The application can be run on a desktop computer or a mobile device and can be accessed through a user-friendly interface.
-
-        The Golden Apple Snail Eggs Detection Application has the potential to revolutionize the way farmers detect and prevent golden apple snail infestations. By providing a fast and accurate way to detect the presence of snail eggs, the application can help farmers save time and money and reduce the use of harmful pesticides.
-        """)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
